4b0810c2fe23ed4c3cc70dd361d9e2ac3c1c4235
kenneth.s.brooks <kenneth.s.brooks@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-02-27 04:13:49 +0000

    added easyb intellij codestyle drop this file into (on mac) ~/Library/Preferences/IntelliJIDEA70/codestyles ran the auto style on SpecificationRunner as an example
    git-svn-id: http://easyb.googlecode.com/svn/trunk@91 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/java/org/disco/easyb/SpecificationRunner.java
+++ src/java/org/disco/easyb/SpecificationRunner.java
@@ -26,7 +26,6 @@ import org.disco.easyb.core.result.Result;
 import org.disco.easyb.core.util.ReportFormat;
 import org.disco.easyb.core.util.ReportType;
 
-
 /**
  * usage is:
  * <p/>
@@ -42,203 +41,200 @@ import org.disco.easyb.core.util.ReportType;
  */
 public class SpecificationRunner {
 
+    List<Report> reports;
 
-  List<Report> reports;
+    public SpecificationRunner() {
+        this(null);
+    }
 
-  public SpecificationRunner() {
-    this(null);
-  }
+    public SpecificationRunner(List<Report> reports) {
+        this.reports = addDefaultReports(reports);
+    }
 
-  public SpecificationRunner(List<Report> reports) {
-    this.reports = addDefaultReports(reports);
-  }
+    /**
+     * TODO: refactor me please
+     *
+     * @param specs collection of files that contain the specifications
+     * @return BehaviorListener has status about failures and successes
+     * @throws Exception if unable to write report file
+     */
+    public void runSpecification(Collection<File> specs) throws Exception {
 
-  /**
-   * TODO: refactor me please
-   * @param specs collection of files that contain the specifications
-   * @throws Exception if unable to write report file
-   * @return BehaviorListener has status about failures and successes
-   */
-  public void runSpecification(Collection<File> specs) throws Exception {
+        List<SpecificationListener> listeners = new ArrayList<SpecificationListener>();
 
-    List<SpecificationListener> listeners = new ArrayList<SpecificationListener>();
+        SpecificationListener listener = new DefaultListener();
 
-    SpecificationListener listener = new DefaultListener();
-
-    for (File file : specs) {
-    	listener.setSpecificationName(file.getName());
-        Specification specification = new Specification(file);
-        if (specification.isStory()) {
-            listener.gotResult(new Result(specification.getPhrase(), SpecificationBinding.STORY, Result.SUCCEEDED));
-        } else {
-            warnOnBehaviorNaming(file);
+        for (File file : specs) {
+            listener.setSpecificationName(file.getName());
+            Specification specification = new Specification(file);
+            if (specification.isStory()) {
+                listener.gotResult(new Result(specification.getPhrase(), SpecificationBinding.STORY, Result.SUCCEEDED));
+            } else {
+                warnOnBehaviorNaming(file);
+            }
+            new GroovyShell(SpecificationBinding.getBinding(listener)).evaluate(file);
         }
-        new GroovyShell(SpecificationBinding.getBinding(listener)).evaluate(file);
-    }
 
-    for(Report report : reports) {
-      ReportWriter reportWriter;
-      if(report.getFormat().concat(report.getType()).equals(Report.TXT_STORY)) {
-        reportWriter = new TxtStoryReportWriter(report, listener);
-      } else if(report.getFormat().concat(report.getType()).equals(Report.XML_BEHAVIOR)) {
-        reportWriter = new XmlBehaviorReportWriter(report, listener);
-      } else {
-        reportWriter = new TerseReportWriter(report, listener);
-      }
+        for (Report report : reports) {
+            ReportWriter reportWriter;
+            if (report.getFormat().concat(report.getType()).equals(Report.TXT_STORY)) {
+                reportWriter = new TxtStoryReportWriter(report, listener);
+            } else if (report.getFormat().concat(report.getType()).equals(Report.XML_BEHAVIOR)) {
+                reportWriter = new XmlBehaviorReportWriter(report, listener);
+            } else {
+                reportWriter = new TerseReportWriter(report, listener);
+            }
 
-      reportWriter.writeReport();
-    }
+            reportWriter.writeReport();
+        }
 
-    listeners.add(listener);
-    
-    int totalSpecificationFailures = 0;
-    for(SpecificationListener ilistener : listeners) {
-      if(ilistener.hasBehaviorFailures()) {
-        totalSpecificationFailures++;
-      }
+        listeners.add(listener);
+
+        int totalSpecificationFailures = 0;
+        for (SpecificationListener ilistener : listeners) {
+            if (ilistener.hasBehaviorFailures()) {
+                totalSpecificationFailures++;
+            }
+        }
+        if (totalSpecificationFailures > 0) {
+            System.out.println("specification failures detected!");
+            System.exit(-6);
+        }
     }
-    if(totalSpecificationFailures > 0) {
-      System.out.println("specification failures detected!");
-      System.exit(-6);
-    }
-  }
 
     private void warnOnBehaviorNaming(File file) {
-        if(!file.getName().contains("Behavior.groovy")){
-        		System.out.println("You should consider ending your specification file (" +
+        if (!file.getName().contains("Behavior.groovy")) {
+            System.out.println("You should consider ending your specification file (" +
                 file.getName() + ") with either Story or Behavior. " +
                 "See easyb documentation for more details. ");
         }
     }
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
-    Options options = getOptionsForMain();
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Options options = getOptionsForMain();
 
-    try {
-        CommandLine commandLine = getCommandLineForMain(args, options);
-        validateArguments(commandLine);
+        try {
+            CommandLine commandLine = getCommandLineForMain(args, options);
+            validateArguments(commandLine);
 
-        SpecificationRunner runner = new SpecificationRunner(getConfiguredReports(commandLine));
+            SpecificationRunner runner = new SpecificationRunner(getConfiguredReports(commandLine));
 
-        runner.runSpecification(getFileCollection(commandLine.getArgs()));
-
-    } catch (IllegalArgumentException iae) {
-      System.out.println(iae.getMessage());
-      handleHelpForMain(options);
-    } catch (ParseException pe) {
-      System.out.println(pe.getMessage());
-      handleHelpForMain(options);
-    } catch (Exception e) {
-      System.err.println("There was an error running the script");
-      e.printStackTrace(System.err);
-      System.exit(-6);
-    }
-  }
-
-  private static void validateArguments(CommandLine commandLine) throws IllegalArgumentException {
-    if(commandLine.getArgs().length == 0) {
-      throw new IllegalArgumentException("Required Arguments not passed in.");
-    }
-  }
-
-  private static List<Report> getConfiguredReports(CommandLine line) {
-
-    List<Report> configuredReports = new ArrayList<Report>();
-    if (line.hasOption(Report.XML_BEHAVIOR)) {
-      Report report = new Report();
-      report.setFormat(ReportFormat.XML.format());
-      if(line.getOptionValue(Report.XML_BEHAVIOR) == null) {
-        report.setLocation("easyb-behavior-report.xml");
-      } else {
-        report.setLocation(line.getOptionValue(Report.XML_BEHAVIOR));
-      }
-      report.setType(ReportType.BEHAVIOR.type());
-
-      configuredReports.add(report);
+            runner.runSpecification(getFileCollection(commandLine.getArgs()));
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+            handleHelpForMain(options);
+        } catch (ParseException pe) {
+            System.out.println(pe.getMessage());
+            handleHelpForMain(options);
+        } catch (Exception e) {
+            System.err.println("There was an error running the script");
+            e.printStackTrace(System.err);
+            System.exit(-6);
+        }
     }
 
-    if(line.hasOption(Report.TXT_STORY)) {
-      Report report = new Report();
-      report.setFormat(ReportFormat.TXT.format());
-      if(line.getOptionValue(Report.TXT_STORY) == null) {
-        report.setLocation("easyb-story-report.txt");
-      } else {
-        report.setLocation(line.getOptionValue(Report.TXT_STORY));
-      }
-      report.setType(ReportType.STORY.type());
-
-      configuredReports.add(report);
-    }
-    return configuredReports;
-  }
-
-  /**
-   * @param paths locations of the specifications to be loaded
-   * @return collection of files where the only element is the file of the spec to be run
-   */
-  private static Collection<File> getFileCollection(String[] paths) {
-    Collection<File> coll = new ArrayList<File>();
-    for (String path : paths) {
-      coll.add(new File(path));
-    }
-    return coll;
-  }
-
-  /**
-   * @param options options that are available to this specification runner
-   */
-  private static void handleHelpForMain(Options options) {
-    new HelpFormatter().printHelp("SpecificationRunner my/path/to/MyStory.groovy",options);
-  }
-
-  /**
-   * @param args    command line arguments passed into main
-   * @param options options that are available to this specification runner
-   * @return representation of command line arguments passed in that match the available options
-   * @throws ParseException if there are any problems encountered while parsing the command line tokens
-   */
-  private static CommandLine getCommandLineForMain(String[] args, Options options) throws ParseException {
-    CommandLineParser commandLineParser = new GnuParser();
-    return commandLineParser.parse(options, args);
-  }
-
-  /**
-   * @return representation of a collection of Option objects, which describe the possible options for a command-line.
-   */
-  private static Options getOptionsForMain() {
-    Options options = new Options();
-
-    //noinspection AccessStaticViaInstance
-    Option xmlbehaviorreport = OptionBuilder.withArgName("file").hasArg()
-        .withDescription("create a behavior report in xml format").create(Report.XML_BEHAVIOR);
-    options.addOption(xmlbehaviorreport);
-
-    //noinspection AccessStaticViaInstance
-    Option storyreport = OptionBuilder.withArgName("file").hasArg()
-        .withDescription("create a story report").create(Report.TXT_STORY);
-    options.addOption(storyreport);
-
-    return options;
-
-  }
-
-  private List<Report> addDefaultReports(List<Report> userConfiguredReports) {
-    List<Report> configuredReports = new ArrayList<Report>();
-
-    if(userConfiguredReports != null) {
-      configuredReports.addAll(userConfiguredReports);
+    private static void validateArguments(CommandLine commandLine) throws IllegalArgumentException {
+        if (commandLine.getArgs().length == 0) {
+            throw new IllegalArgumentException("Required Arguments not passed in.");
+        }
     }
 
-    Report terseStoryReport = new Report();
-    terseStoryReport.setFormat(ReportFormat.TERSE.format());
-    terseStoryReport.setLocation("screen");
-    terseStoryReport.setType(ReportType.STORY.type());
-    configuredReports.add(terseStoryReport);
+    private static List<Report> getConfiguredReports(CommandLine line) {
 
-    return configuredReports;
-  }
+        List<Report> configuredReports = new ArrayList<Report>();
+        if (line.hasOption(Report.XML_BEHAVIOR)) {
+            Report report = new Report();
+            report.setFormat(ReportFormat.XML.format());
+            if (line.getOptionValue(Report.XML_BEHAVIOR) == null) {
+                report.setLocation("easyb-behavior-report.xml");
+            } else {
+                report.setLocation(line.getOptionValue(Report.XML_BEHAVIOR));
+            }
+            report.setType(ReportType.BEHAVIOR.type());
 
+            configuredReports.add(report);
+        }
+
+        if (line.hasOption(Report.TXT_STORY)) {
+            Report report = new Report();
+            report.setFormat(ReportFormat.TXT.format());
+            if (line.getOptionValue(Report.TXT_STORY) == null) {
+                report.setLocation("easyb-story-report.txt");
+            } else {
+                report.setLocation(line.getOptionValue(Report.TXT_STORY));
+            }
+            report.setType(ReportType.STORY.type());
+
+            configuredReports.add(report);
+        }
+        return configuredReports;
+    }
+
+    /**
+     * @param paths locations of the specifications to be loaded
+     * @return collection of files where the only element is the file of the spec to be run
+     */
+    private static Collection<File> getFileCollection(String[] paths) {
+        Collection<File> coll = new ArrayList<File>();
+        for (String path : paths) {
+            coll.add(new File(path));
+        }
+        return coll;
+    }
+
+    /**
+     * @param options options that are available to this specification runner
+     */
+    private static void handleHelpForMain(Options options) {
+        new HelpFormatter().printHelp("SpecificationRunner my/path/to/MyStory.groovy", options);
+    }
+
+    /**
+     * @param args    command line arguments passed into main
+     * @param options options that are available to this specification runner
+     * @return representation of command line arguments passed in that match the available options
+     * @throws ParseException if there are any problems encountered while parsing the command line tokens
+     */
+    private static CommandLine getCommandLineForMain(String[] args, Options options) throws ParseException {
+        CommandLineParser commandLineParser = new GnuParser();
+        return commandLineParser.parse(options, args);
+    }
+
+    /**
+     * @return representation of a collection of Option objects, which describe the possible options for a command-line.
+     */
+    private static Options getOptionsForMain() {
+        Options options = new Options();
+
+        //noinspection AccessStaticViaInstance
+        Option xmlbehaviorreport = OptionBuilder.withArgName("file").hasArg()
+            .withDescription("create a behavior report in xml format").create(Report.XML_BEHAVIOR);
+        options.addOption(xmlbehaviorreport);
+
+        //noinspection AccessStaticViaInstance
+        Option storyreport = OptionBuilder.withArgName("file").hasArg()
+            .withDescription("create a story report").create(Report.TXT_STORY);
+        options.addOption(storyreport);
+
+        return options;
+    }
+
+    private List<Report> addDefaultReports(List<Report> userConfiguredReports) {
+        List<Report> configuredReports = new ArrayList<Report>();
+
+        if (userConfiguredReports != null) {
+            configuredReports.addAll(userConfiguredReports);
+        }
+
+        Report terseStoryReport = new Report();
+        terseStoryReport.setFormat(ReportFormat.TERSE.format());
+        terseStoryReport.setLocation("screen");
+        terseStoryReport.setType(ReportType.STORY.type());
+        configuredReports.add(terseStoryReport);
+
+        return configuredReports;
+    }
 }
