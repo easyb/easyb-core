20fd646bd7c5d03e7bbf1d7192a30089b40cd588
ajglover <ajglover@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-05-29 01:37:45 +0000

    updated provider interface stuff
    git-svn-id: http://easyb.googlecode.com/svn/trunk@349 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/groovy/org/disco/easyb/delegates/PlugableDelegate.groovy
+++ src/groovy/org/disco/easyb/delegates/PlugableDelegate.groovy
@@ -3,23 +3,21 @@ package org.disco.easyb.delegates
 import javax.imageio.spi.ServiceRegistry
 
 class PlugableDelegate {
-  /**
-   *
-   */
-  def invokeMethod(String method, Object args) {
-    Iterator providers = ServiceRegistry.lookupProviders(Plugable.class, ClassLoader.getSystemClassLoader())
-    def found = false
-    while (providers.hasNext() && !found) {
-      Object provider = providers.next()
-      provider.getClass().getMethods().each {
-        if (it.getName().equals(method)) {
-          provider.invokeMethod(method, args)
-          found = true
+    /**
+     *
+     */
+    def invokeMethod(String method, Object args) {
+        Iterator providers = ServiceRegistry.lookupProviders(Plugable.class, ClassLoader.getSystemClassLoader())
+        def found = false
+        while (providers.hasNext() && !found) {
+            Object provider = providers.next()
+            if (provider.metaClass.respondsTo(provider, method, args)) {
+                provider.invokeMethod(method, args)
+                found = true
+            }
         }
-      }
+        if (!found) {
+            throw new MissingMethodException(method, PlugableDelegate.class, args)
+        }
     }
-    if (!found) {
-      throw new MissingMethodException(method, PlugableDelegate.class, args)
-    }
-  }
 }
