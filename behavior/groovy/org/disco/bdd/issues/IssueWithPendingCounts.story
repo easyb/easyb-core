f2337859c2cb94e03c6a26a3f35cc3d04b847f5f
ajglover <ajglover@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-06-28 22:42:13 +0000

    fixing non-pending reporting issue
    git-svn-id: http://easyb.googlecode.com/svn/trunk@377 9bebd20b-ac44-0410-9e34-335f4663fabf

--- behavior/groovy/org/disco/bdd/issues/IssueWithPendingCounts.story
+++ behavior/groovy/org/disco/bdd/issues/IssueWithPendingCounts.story
@@ -1,3 +1,19 @@
+description """
+Ideally, easyb should report scenarios/stories that are pending; thus,
+the following three scenarios would yield a report that contains
+3 pending scenarios. For example, the output of the console should be
+something like:
+
+Running issue with pending counts story (IssueWithPendingCounts.story)
+Scenarios run: 3, Failures: 0, Pending: 3, Time Elapsed: 0.402 sec
+
+3 total behaviors run with no failures
+
+And the text report would have a [pending] flag next to each scenario or
+then clause
+
+"""
+
 scenario "this is a pending scenario", {
     given "something"
     when "something is done"
@@ -11,5 +27,4 @@ scenario "this is another pending scenario", {
     then "there is also a way to handle it"
 }
 
-//this works
 scenario "this is finally a pending scenario with nothing in it"
--- src/groovy/org/disco/easyb/report/TxtStoryReportWriter.groovy
+++ src/groovy/org/disco/easyb/report/TxtStoryReportWriter.groovy
@@ -1,119 +1,123 @@
 package org.disco.easyb.report
 
+import org.disco.easyb.listener.ResultsCollector
 import org.disco.easyb.result.Result
 import org.disco.easyb.util.BehaviorStepType
-import org.disco.easyb.listener.ResultsCollector
 
 class TxtStoryReportWriter implements ReportWriter {
-  private String location
-  private static final String DEFAULT_LOC_NAME = "easyb-story-report.txt";
+    private String location
+    private static final String DEFAULT_LOC_NAME = "easyb-story-report.txt";
 
-  public TxtStoryReportWriter() {
-    this(DEFAULT_LOC_NAME)
-  }
-
-  public TxtStoryReportWriter(String location) {
-    this.location = (location != null? location : DEFAULT_LOC_NAME);
-  }
-
-  /**
-   * Render a text story report
-   */
-  void writeReport(ResultsCollector results) {
-    Writer writer = new BufferedWriter(new FileWriter(new File(location)))
-    def count = results.scenarioCount
-    writer.writeLine("${(count > 1) ? "${count} scenarios" : " 1 scenario"}" +
-            " (including ${results.pendingScenarioCount} pending) executed" +
-            "${results.failedScenarioCount.toInteger() > 0 ? ", but status is failure!" : " successfully"}" +
-            "${results.failedScenarioCount.toInteger() > 0 ? " Total failures: ${results.failedScenarioCount}" : ""}")
-    results.genesisStep.getChildrenOfType(BehaviorStepType.STORY).each {genesisChild ->
-      handleElement(writer, genesisChild)
+    public TxtStoryReportWriter() {
+        this(DEFAULT_LOC_NAME)
     }
 
-    writer.close()
-  }
-
-  /**
-   *
-   */
-  def handleElement(writer, element) {
-    writeStep(writer, element)
-    element.getChildSteps().each {
-      writeStep(writer, it)
+    public TxtStoryReportWriter(String location) {
+        this.location = (location != null ? location : DEFAULT_LOC_NAME);
     }
-  }
 
-  /**
-   *
-   */
-  def writeStep(writer, element) {
-
-    switch (element.stepType) {
-      case BehaviorStepType.STORY:
-        writer.newLine()
-        writer.write("${' '.padRight(2)}Story: ${element.name}")
-        break
-      case BehaviorStepType.DESCRIPTION:
-        writer.write("${' '.padRight(3)} ${element.description}")
-        writer.newLine()
-        break
-      case BehaviorStepType.SCENARIO:
-        writer.newLine()
-        writer.write("${' '.padRight(4)}scenario ${element.name}")
-        element.getChildSteps().each {
-          writer.newLine()
-          switch (it.stepType) {
-            case BehaviorStepType.GIVEN:
-              writer.write("${' '.padRight(6)}given ${it.name}")
-              break
-            case BehaviorStepType.WHEN:
-              writer.write("${' '.padRight(6)}when ${it.name}")
-              break
-            case BehaviorStepType.THEN:
-              writer.write("${' '.padRight(6)}then ${it.name}")
-              break
-            case BehaviorStepType.AND:
-              writer.write("${' '.padRight(6)}and")
-              break
-            default:
-              //no op to avoid having alerts in story text
-              break
-          }
-          if (it.result?.pending()) {
-            writer.write(" [PENDING]")
-          } else if (it.result?.failed()) {
-            writer.write(" [FAILURE: ${it.result?.cause()?.getMessage()}]")
-          }
+    /**
+     * Render a text story report
+     */
+    void writeReport(ResultsCollector results) {
+        Writer writer = new BufferedWriter(new FileWriter(new File(location)))
+        def count = results.scenarioCount
+        writer.writeLine("${(count > 1) ? "${count} scenarios" : " 1 scenario"}" +
+                " (including ${results.pendingScenarioCount} pending) executed" +
+                "${results.failedScenarioCount.toInteger() > 0 ? ", but status is failure!" : " successfully"}" +
+                "${results.failedScenarioCount.toInteger() > 0 ? " Total failures: ${results.failedScenarioCount}" : ""}")
+        results.genesisStep.getChildrenOfType(BehaviorStepType.STORY).each {genesisChild ->
+            handleElement(writer, genesisChild)
         }
-        break
-      case BehaviorStepType.GIVEN:
-        writer.write("${' '.padRight(6)}given ${element.name}")
-        break
-      case BehaviorStepType.WHEN:
-        writer.write("${' '.padRight(6)}when ${element.name}")
-        break
-      case BehaviorStepType.THEN:
-        writer.write("${' '.padRight(6)}then ${element.name}")
-        break
-      case BehaviorStepType.AND:
-        writer.write("${' '.padRight(6)}and")
-        break
-      default:
-        //no op to avoid having alerts in story text
-        break
+
+        writer.close()
     }
 
-    if (element.result == Result.FAILED) {
-      writer.newLine()
-      writer.newLine()
-      writer.write("	Failure -> ${element.name} ${element.description}")
-      writer.newLine()
-      writer.write("${element.failuremessage}")
+    /**
+     *
+     */
+    def handleElement(writer, element) {
+        writeStep(writer, element)
+        element.getChildSteps().each {
+            writeStep(writer, it)
+        }
     }
-    if (element.result == Result.PENDING) {
-      writer.write(" [PENDING]")
-    }
-    writer.newLine()
 
-  }
+    /**
+     *
+     */
+    def writeStep(writer, element) {
+
+        switch (element.stepType) {
+            case BehaviorStepType.STORY:
+                writer.newLine()
+                writer.write("${' '.padRight(2)}Story: ${element.name}")
+                break
+            case BehaviorStepType.DESCRIPTION:
+                writer.write("${' '.padRight(3)} ${element.description}")
+                writer.newLine()
+                break
+            case BehaviorStepType.SCENARIO:
+                writer.newLine()
+                writer.write("${' '.padRight(4)}scenario ${element.name}")
+                element.getChildSteps().each {
+                    writer.newLine()
+                    switch (it.stepType) {
+                        case BehaviorStepType.GIVEN:
+                            writer.write("${' '.padRight(6)}given ${it.name}")
+                            break
+                        case BehaviorStepType.WHEN:
+                            writer.write("${' '.padRight(6)}when ${it.name}")
+                            break
+                        case BehaviorStepType.THEN:
+                            writer.write("${' '.padRight(6)}then ${it.name}")
+                            break
+                        case BehaviorStepType.AND:
+                            writer.write("${' '.padRight(6)}and")
+                            break
+                        default:
+                            //no op to avoid having alerts in story text
+                            break
+                    } //end it.stepType switch
+                    if (it.result?.pending()) {
+                        writer.write(" [PENDING]")
+                    } else if (it.result?.failed()) {
+                        writer.write(" [FAILURE: ${it.result?.cause()?.getMessage()}]")
+                    }
+                }//end child steps
+                if (element.getChildSteps().size == 0) {
+                    //a scenario w/out child steps is pending
+                    writer.write(" [PENDING]")
+                }
+                break
+            case BehaviorStepType.GIVEN:
+                writer.write("${' '.padRight(6)}given ${element.name}")
+                break
+            case BehaviorStepType.WHEN:
+                writer.write("${' '.padRight(6)}when ${element.name}")
+                break
+            case BehaviorStepType.THEN:
+                writer.write("${' '.padRight(6)}then ${element.name}")
+                break
+            case BehaviorStepType.AND:
+                writer.write("${' '.padRight(6)}and")
+                break
+            default:
+                //no op to avoid having alerts in story text
+                break
+        }
+
+        if (element.result == Result.FAILED) {
+            writer.newLine()
+            writer.newLine()
+            writer.write("	Failure -> ${element.name} ${element.description}")
+            writer.newLine()
+            writer.write("${element.failuremessage}")
+        }
+        if (element.result == Result.PENDING) {
+            writer.write(" [PENDING]")
+        }
+        writer.newLine()
+
+    }
 }
