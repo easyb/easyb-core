2bc48da0f127efc2415497ad59399c84b7b447cf
ajglover <ajglover@9bebd20b-ac44-0410-9e34-335f4663fabf>
2008-11-20 02:57:53 +0000

    new stack trace types
    git-svn-id: http://easyb.googlecode.com/svn/trunk@551 9bebd20b-ac44-0410-9e34-335f4663fabf

---
+++ src/java/org/disco/easyb/FilteredStackTraceConsoleReporter.java
@@ -0,0 +1,34 @@
+package org.disco.easyb;
+
+import org.disco.easyb.result.Result;
+
+public class FilteredStackTraceConsoleReporter extends ConsoleReporter {
+    public void gotResult(Result result) {
+        super.gotResult(result);
+        if (result != null && result.failed()
+                && result.cause() != null) {
+            StackTraceElement[] stacktrace = result.cause().getStackTrace();
+            System.out.println("\t An exception has occurred: ");
+            for (int x = 0; x < stacktrace.length; x++) {
+                if (lineOkToPrint(stacktrace[x].toString())) {
+                    System.out.println("\t\t" + stacktrace[x]);
+                }
+            }
+        }
+    }
+
+    private boolean lineOkToPrint(String line) {
+        String trimmed = line.trim();
+        if (trimmed.startsWith("org.codehaus.groovy") ||
+                trimmed.startsWith("groovy") ||
+                trimmed.startsWith("sun.reflect") ||
+                trimmed.startsWith("java.lang.reflect") ||
+                trimmed.startsWith("org.apache.tools.ant") ||
+                trimmed.startsWith("org.codehaus.gant") ||
+                trimmed.startsWith("gant.Gant") ||
+                trimmed.startsWith("build_gant$")) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/java/org/disco/easyb/StackTraceConsoleReporter.java
@@ -0,0 +1,20 @@
+package org.disco.easyb;
+
+import org.disco.easyb.result.Result;
+
+public class StackTraceConsoleReporter extends ConsoleReporter {
+
+    public void gotResult(Result result) {
+        super.gotResult(result);
+
+        if (result != null && result.failed()
+                && result.cause() != null) {
+            StackTraceElement[] stacktrace = result.cause().getStackTrace();
+            System.out.println("\t An exception has occurred: ");
+            for (int x = 0; x < stacktrace.length; x++) {
+                System.out.println("\t\t" + stacktrace[x]);
+            }
+        }
+
+    }
+}
