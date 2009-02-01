71056c6707a9c4935d4b051d38c90190cfbc1dfb
ajglover <ajglover@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-02-01 03:58:02 +0000

    added error message parameter
    git-svn-id: http://easyb.googlecode.com/svn/trunk@589 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/groovy/org/disco/easyb/BehaviorCategory.groovy
+++ src/groovy/org/disco/easyb/BehaviorCategory.groovy
@@ -6,127 +6,214 @@ import org.disco.easyb.exception.VerificationException
 class BehaviorCategory {
 
     static void shouldBeGreaterThan(Object self, value) {
+        shouldBeGreaterThan(self, value, null)
+    }
+
+    static void shouldBeGreaterThan(Object self, value, String msg) {
         if (value > self) {
-            throw new VerificationException("${value} was greater than ${self}")
+            def out = "${value} was greater than ${self}"
+            throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
+
         } else if (value == self) {
-            throw new VerificationException("${value} was equal to ${self}")
+            def out = "${value} was equal to ${self}"
+            throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
+        }
+    }
+
+    static void shouldBeLessThan(Object self, value, String msg) {
+        if ((self.getClass() == NullObject.class) || (value == null)) {
+            def out = "${value} can not be compared to ${self}"
+            throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
+
+        } else if (value < self) {
+            def out = "${value} was less than ${self}"
+            throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
+
+        } else if (value == self) {
+            def out = "${value} was equal to ${self}"
+            throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
         }
     }
 
     static void shouldBeLessThan(Object self, value) {
-        if ((self.getClass() == NullObject.class) || (value == null)) {
-            throw new VerificationException("${value} can not be compared to ${self}")
-        } else if (value < self) {
-            throw new VerificationException("${value} was less than ${self}")
-        } else if (value == self) {
-            throw new VerificationException("${value} was equal to ${self}")
-        }
+        shouldBeLessThan(self, value, null)
     }
 
     static void shouldBeEqual(Object self, value) {
-        _equals(self, value)
+        shouldBe(self, value, null)
+    }
+
+    static void shouldBeEqual(Object self, value, String msg) {
+        shouldBe(self, value, msg)
     }
 
     static void shouldBeEqualTo(Object self, value) {
-        _equals(self, value)
+        shouldBe(self, value, null)
     }
 
-    static void shouldBe(Object self, value) {
-        _equals(self, value)
+    static void shouldBeEqualTo(Object self, value, String msg) {
+        shouldBe(self, value, msg)
     }
 
     static void shouldEqual(Object self, value) {
-        _equals(self, value)
+        shouldBe(self, value, null)
     }
 
-    private static void _equals(self, value) {
+    static void shouldEqual(Object self, value, String msg) {
+        shouldBe(self, value, msg)
+    }
+
+    static void shouldBe(Object self, value) {
+        shouldBe(self, value, null)
+    }
+
+    static void shouldBe(Object self, value, String msg) {
+        isEqual(self, value, msg)
+    }
+
+    private static void isEqual(self, value, String msg) {
         if (self.getClass() == NullObject.class) {
             if (value != null) {
-                throw new VerificationException("expected ${value.toString()} but target object is null")
+                def out = "expected ${value.toString()} but target object is null"
+                throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
             }
         } else if (value.getClass() == String.class) {
             if (!value.toString().equals(self.toString())) {
-                throw new VerificationException("expected ${value.toString()} but was ${self.toString()}")
+                def out = "expected ${value.toString()} but was ${self.toString()}"
+                throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
             }
         } else {
             if (value != self) {
-                throw new VerificationException("expected ${value} but was ${self}")
+                def out = "expected ${value} but was ${self}"
+                throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
             }
         }
     }
 
+    static void shouldNotBe(Object self, value, String msg) {
+        notEquals(self, value, msg)
+    }
+
+    static void shouldNotEqual(Object self, value, String msg) {
+        shouldNotBe(self, value, msg)
+    }
+
+    static void shouldntBe(Object self, value, String msg) {
+        shouldNotBe(self, value, msg)
+    }
+
+    static void shouldntEqual(Object self, value, String msg) {
+        shouldNotBe(self, value, msg)
+    }
+
     static void shouldNotBe(Object self, value) {
-        _notEquals(self, value)
+        shouldNotBe(self, value, null)
     }
 
     static void shouldNotEqual(Object self, value) {
-        _notEquals(self, value)
+        shouldNotBe(self, value, null)
     }
 
     static void shouldntBe(Object self, value) {
-        _notEquals(self, value)
+        shouldNotBe(self, value, null)
     }
 
     static void shouldntEqual(Object self, value) {
-        _notEquals(self, value)
+        shouldNotBe(self, value, null)
     }
 
-    private static void _notEquals(self, value) {
+    private static void notEquals(self, value, String msg) {
         if (self?.getClass() == String.class) {
             if (value.toString().equals(self.toString())) {
-                throw new VerificationException("expected values to differ but both were ${value.toString()}")
+                def out = "expected values to differ but both were ${value.toString()}"
+                throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
             }
         } else {
             if (value == self) {
-                throw new VerificationException("expected values to differ but both were ${value}")
+                def out = "expected values to differ but both were ${value}"
+                throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
             } else {
                 if (self.is(null) && value.is(null)) {
-                    throw new VerificationException("expected values to differ but both were null")
+                    def out = "expected values to differ but both were null"
+                    throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
+
                 }
             }
         }
     }
 
-    private static void _isA(Object self, type) {
+
+    private static void isA(Object self, type) {
+        isA(self, type, null)
+    }
+
+    private static void isNotA(Object self, type) {
+        isNotA(self, type, null)
+    }
+
+    private static void isA(Object self, type, String msg) {
         if (!type.equals(self.getClass())) {
-            throw new VerificationException("expected ${type} but was ${self.getClass()}")
+            def out = "expected ${type} but was ${self.getClass()}"
+            throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
         }
     }
 
-    private static void _isNotA(Object self, type) {
+    private static void isNotA(Object self, type, String msg) {
         if (type.equals(self.getClass())) {
-            throw new VerificationException("expected ${type} but was ${self.getClass()}")
+            def out = "expected ${type} but was ${self.getClass()}"
+            throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
         }
     }
 
     static void shouldBeA(Object self, value) {
-        _isA(self, value)
+        shouldBeA(self, value, null)
+    }
+
+    static void shouldBeA(Object self, value, String msg) {
+        isA(self, value, msg)
     }
 
     static void shouldBeAn(Object self, value) {
-        _isA(self, value)
+        shouldBeA(self, value, null)
+    }
+
+    static void shouldBeAn(Object self, value, String msg) {
+        shouldBeA(self, value, msg)
     }
 
     static void shouldNotBeA(Object self, value) {
-        _isNotA(self, value)
+        shouldNotBeA(self, value, null)
+    }
+
+    static void shouldNotBeA(Object self, value, String msg) {
+        isNotA(self, value, msg)
     }
 
     static void shouldNotBeAn(Object self, value) {
-        _isNotA(self, value)
+        shouldNotBeA(self, value, null)
     }
 
+    static void shouldNotBeAn(Object self, value, String msg) {
+        shouldNotBeA(self, value, msg)
+    }
+
+//    def out = "expected ${value.toString()} but target object is null"
+//    throw new VerificationException((msg != null ? "\"" + msg + "\", " + out : out))
+
+
+
     static void shouldHave(Object self, value) {
-        _has(self, value)
+        has(self, value)
     }
 
     static void shouldNotHave(Object self, value) {
-        _hasNot(self, value)
+        hasNot(self, value)
     }
 
-    private static void _hasNot(Object self, value) {
+    private static void hasNot(Object self, value) {
         if (self instanceof Map) {
             if (value instanceof Map) {
-                _handleMapShouldNotContain(self, value)
+                handleMapShouldNotContain(self, value)
             } else {
                 if (self.containsKey(value) || self.containsValue(value)) {
                     throw new VerificationException("${self.toString()} should not contain ${value.toString()} as a key or value")
@@ -166,11 +253,11 @@ class BehaviorCategory {
         }
     }
 
-    private static void _has(Object self, value) {
+    private static void has(Object self, value) {
 
         if (self instanceof Map) {
             if (value instanceof Map) {
-                _handleMapContains(self, value)
+                handleMapContains(self, value)
 
             } else {
                 if (!self.containsKey(value)) {
@@ -223,7 +310,7 @@ class BehaviorCategory {
         return foundcount
     }
 
-    private static void _handleMapShouldNotContain(delegate, values) {
+    private static void handleMapShouldNotContain(delegate, values) {
         def foundcount = findItems(delegate, values)
         if (foundcount == values.size()) {
             throw new VerificationException("values ${values} found in ${delegate}")
@@ -231,7 +318,7 @@ class BehaviorCategory {
     }
 
 
-    private static void _handleMapContains(delegate, values) {
+    private static void handleMapContains(delegate, values) {
         def foundcount = findItems(delegate, values)
         if (foundcount != values.size()) {
             throw new VerificationException("values ${values} not found in ${delegate}")
