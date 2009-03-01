be1f09cea9c79c1a89607768b71d8c1f58f8e92e
ajglover <ajglover@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-03-01 17:08:23 +0000

    more pckg
    git-svn-id: http://easyb.googlecode.com/svn/trunk@627 9bebd20b-ac44-0410-9e34-335f4663fabf

---
+++ src/ant/org/easyb/ant/BehaviorRunnerTask.groovy
@@ -0,0 +1,27 @@
+package org.easyb.ant
+
+import org.apache.tools.ant.types.FileSet
+import org.easyb.BehaviorRunner
+
+public class BehaviorRunnerTask extends AbstractStoryTask {
+
+    public BehaviorRunnerTask() {
+        this(new CommandRunnerImpl())
+    }
+
+    public BehaviorRunnerTask(CommandRunner runner) {
+        super(BehaviorRunner.class, runner)
+    }
+
+    public void setBehaviorsClassName(String behaviorsClassName) {
+        addTarget(behaviorsClassName)
+    }
+
+    public void addBehaviors(FileSet fileset) {
+        addFilesetTarget(fileset)
+    }
+
+    void addConfiguredReport(Report report) {
+        addTarget("-${report.format} ${report.location}")
+    }
+}
