652e494cfed1c158dc5557adf80ae6c4d0e1071a
richard.vowles <richard.vowles@9bebd20b-ac44-0410-9e34-335f4663fabf>
2009-10-02 02:51:44 +0000

    fixed offset for specificating that uses file handles
    git-svn-id: http://easyb.googlecode.com/svn/trunk@833 9bebd20b-ac44-0410-9e34-335f4663fabf

--- behavior/groovy/org/easyb/BehaviorNamingSpecification.groovy
+++ behavior/groovy/org/easyb/BehaviorNamingSpecification.groovy
@@ -6,32 +6,34 @@ import org.easyb.domain.BehaviorFactory
 
 //needed for eclipse
 
+File prefix = new File("behavior/groovy/org/easyb")
+
 it "should identify files ending with *Story.groovy as stories", {
-    behavior = BehaviorFactory.createBehavior(new File('EmptyStackStory.groovy'))
+    behavior = BehaviorFactory.createBehavior(new File(prefix, 'EnsuringHasStory.groovy'))
     (behavior instanceof Story).shouldBe(true)
-    behavior.phrase.shouldBe('empty stack')
+    behavior.phrase.shouldBe('ensuring has')
 }
 
 it "should identify files ending with *.story as stories", {
-    behavior = BehaviorFactory.createBehavior(new File('EmptyStack.story'))
+    behavior = BehaviorFactory.createBehavior(new File(prefix, 'SharedBehaviors.story'))
     (behavior instanceof Story).shouldBe(true)
-    behavior.phrase.shouldBe('empty stack')
+    behavior.phrase.shouldBe('shared behaviors')
 }
 
 it "should identify files ending with *Specification.groovy as specifications", {
-    behavior = BehaviorFactory.createBehavior(new File('EmptyStackSpecification.groovy'))
+    behavior = BehaviorFactory.createBehavior(new File(prefix, 'ItClosureSpecification.groovy'))
     (behavior instanceof Specification).shouldBe(true)
-    behavior.phrase.shouldBe('empty stack')
+    behavior.phrase.shouldBe('it closure')
 }
 
 it "should identify files ending with *.specification as specifications", {
-    behavior = BehaviorFactory.createBehavior(new File('EmptyStack.specification'))
+    behavior = BehaviorFactory.createBehavior(new File(prefix, 'WhenStartingBehaviorStep.specification'))
     (behavior instanceof Specification).shouldBe(true)
-    behavior.phrase.shouldBe('empty stack')
+    behavior.phrase.shouldBe('when starting behavior step')
 }
 
 it "should throw an IllegalArgumentException for files not ending in a known format", {
     ensureThrows(IllegalArgumentException.class) {
-        BehaviorFactory.createBehavior(new File('NonConforming.groovy'))
+        BehaviorFactory.createBehavior(new File(prefix, 'Noname.groovy'))
     }
 }
