b98492d123088e5cdd8e013865589ad7d27d92a3
ajglover <ajglover@9bebd20b-ac44-0410-9e34-335f4663fabf>
2010-02-11 20:53:19 +0000

    added categories feature to Ant task
    git-svn-id: http://easyb.googlecode.com/svn/trunk@912 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/ant/org/easyb/ant/AbstractStoryTask.groovy
+++ src/ant/org/easyb/ant/AbstractStoryTask.groovy
@@ -5,136 +5,140 @@ import org.apache.tools.ant.types.Path
 
 public class AbstractStoryTask extends AbstractJavaTask {
 
-    private final Class commandLineClass
-    private List targetArgumentList = new LinkedList()
-    private List filesets = new ArrayList()
-    private String failureProperty
+  private final Class commandLineClass
+  private List targetArgumentList = new LinkedList()
+  private List filesets = new ArrayList()
+  private String failureProperty
 
 
-    public AbstractStoryTask(Class commandLineClass, CommandRunner runner) {
-        super(runner)
-        this.commandLineClass = commandLineClass
+  public AbstractStoryTask(Class commandLineClass, CommandRunner runner) {
+    super(runner)
+    this.commandLineClass = commandLineClass
+  }
+
+  void addTarget(String targetArgument) {
+    targetArgumentList.add(targetArgument)
+  }
+
+  void addTarget(int index, String dir) {
+    targetArgumentList.add(index, dir)
+  }
+
+  void setFailureFile(String filename) {
+    addTarget("-outfail ${filename}")
+  }
+
+  void setInFile(String filename) {
+    addTarget("-f ${filename}")
+  }
+
+  void setNoExecute(boolean exe) {
+    if (exe) {
+      addTarget("-ne")
     }
+  }
 
-    void addTarget(String targetArgument) {
-        targetArgumentList.add(targetArgument)
-    }
+  void setCategories(String cats) {
+    addTarget("-c ${cats}")
+  }
 
-    void addTarget(int index, String dir) {
-        targetArgumentList.add(index, dir)
-    }
+  String getFailureProperty() {
+    return failureProperty
+  }
 
-    void setFailureFile(String filename){
-      addTarget("-outfail ${filename}")
-    }
+  void setFailureProperty(String failureProperty) {
+    this.failureProperty = failureProperty
+  }
 
-    void setInFile(String filename){
-      addTarget("-f ${filename}")
-    }
+  void execute() {
+    appendAntTaskJar()
+    addArgumentsAndRun()
+  }
 
-    void setNoExecute(boolean exe){
-      if(exe){
-        addTarget("-ne")
+  private void appendAntTaskJar() {
+    createClasspath().append(new Path(getProject(), locate()))
+  }
+
+  private void addArgumentsAndRun() {
+
+    for (iter in filesets) {
+
+      def ds = iter.getDirectoryScanner(getProject())
+      def includedFiles = ds.getIncludedFiles()
+
+      if (includedFiles.length == 0) {
+        log("Nothing was found to process. Are your <include> elements defined correctly?")
+        return
       }
+
+      for (i in 0..<includedFiles.length) {
+        addTarget("\"${ds.getBasedir().toString()}/${includedFiles[i]}\"")
+      }
+      log("easyb is preparing to process ${includedFiles.length} file(s)")
     }
 
-    String getFailureProperty() {
-        return failureProperty
+    commandLine.setClassname(commandLineClass.getName())
+
+    for (iter in targetArgumentList) {
+      commandLine.createArgument().setLine(iter)
     }
 
-    void setFailureProperty(String failureProperty) {
-        this.failureProperty = failureProperty
+    if (run() != 0) {
+      log("easyb execution FAILED")
+      if (getFailureProperty() != null) {
+        getProject().setNewProperty(getFailureProperty(), "true");
+      }
+    } else {
+      log("easyb execution passed")
     }
+  }
 
-    void execute() {
-        appendAntTaskJar()
-        addArgumentsAndRun()
+
+  public void addFilesetTarget(FileSet fileset) {
+    filesets.add(fileset)
+  }
+
+  private String locate() {
+    URL url = getClass().getResource(resourcePathToClassFile())
+    if ("jar".equalsIgnoreCase(url.getProtocol())) {
+      return getJarFileOnClassPath(url)
+    } else {
+      return goToClassPathRootDirectory(url)
     }
+  }
 
-    private void appendAntTaskJar() {
-        createClasspath().append(new Path(getProject(), locate()))
+  private String goToClassPathRootDirectory(URL url) {
+    int level = new StringTokenizer(getClass().getName(), ".").countTokens()
+    File directory = getFileOnClassPath(url).getParentFile()
+    for (i in 0..<(level - 1)) {
+      directory = directory.getParentFile()
     }
+    return directory.getAbsolutePath()
+  }
 
-    private void addArgumentsAndRun() {
-
-        for (iter in filesets) {
-
-            def ds = iter.getDirectoryScanner(getProject())
-            def includedFiles = ds.getIncludedFiles()
-
-            if (includedFiles.length == 0) {
-                log("Nothing was found to process. Are your <include> elements defined correctly?")
-                return
-            }
-
-            for (i in 0..<includedFiles.length) {
-                addTarget("\"${ds.getBasedir().toString()}/${includedFiles[i]}\"")
-            }
-            log("easyb is preparing to process ${includedFiles.length} file(s)")
-        }
-
-        commandLine.setClassname(commandLineClass.getName())
-
-        for (iter in targetArgumentList) {
-            commandLine.createArgument().setLine(iter)
-        }
-
-        if (run() != 0) {
-            log("easyb execution FAILED")
-            if (getFailureProperty() != null) {
-                getProject().setNewProperty(getFailureProperty(), "true");
-            }
-        } else {
-            log("easyb execution passed")
-        }
+  private String getJarFileOnClassPath(URL url) {
+    String file = url.getFile()
+    int index = file.indexOf("!")
+    if (index == -1) {
+      throw new IllegalArgumentException(url.toExternalForm() + " does not have '!' for a Jar URL")
     }
+    File fileObject = fromUri(file.substring(0, index))
+    return fileObject.getAbsolutePath()
+  }
 
+  private File getFileOnClassPath(URL url) {
+    return fromUri(url.getFile())
+  }
 
-    public void addFilesetTarget(FileSet fileset) {
-        filesets.add(fileset)
+  private File fromUri(String fileUri) {
+    try {
+      return new File(new URI(fileUri))
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Couldn't locate file:" + fileUri, e)
     }
+  }
 
-    private String locate() {
-        URL url = getClass().getResource(resourcePathToClassFile())
-        if ("jar".equalsIgnoreCase(url.getProtocol())) {
-            return getJarFileOnClassPath(url)
-        } else {
-            return goToClassPathRootDirectory(url)
-        }
-    }
-
-    private String goToClassPathRootDirectory(URL url) {
-        int level = new StringTokenizer(getClass().getName(), ".").countTokens()
-        File directory = getFileOnClassPath(url).getParentFile()
-        for (i in 0..<(level - 1)) {
-            directory = directory.getParentFile()
-        }
-        return directory.getAbsolutePath()
-    }
-
-    private String getJarFileOnClassPath(URL url) {
-        String file = url.getFile()
-        int index = file.indexOf("!")
-        if (index == -1) {
-            throw new IllegalArgumentException(url.toExternalForm() + " does not have '!' for a Jar URL")
-        }
-        File fileObject = fromUri(file.substring(0, index))
-        return fileObject.getAbsolutePath()
-    }
-
-    private File getFileOnClassPath(URL url) {
-        return fromUri(url.getFile())
-    }
-
-    private File fromUri(String fileUri) {
-        try {
-            return new File(new URI(fileUri))
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Couldn't locate file:" + fileUri, e)
-        }
-    }
-
-    private String resourcePathToClassFile() {
-        return "/" + getClass().getName().replace('.', '/') + ".class"
-    }
+  private String resourcePathToClassFile() {
+    return "/" + getClass().getName().replace('.', '/') + ".class"
+  }
 }
