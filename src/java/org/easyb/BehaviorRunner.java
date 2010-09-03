7d6331ee94900a84df79c03ee88d7399bf2d0df0
richard.vowles <richard.vowles@9bebd20b-ac44-0410-9e34-335f4663fabf>
2010-09-03 09:13:56 +0000

    intelllij plugin detected that completeTesting was not being called (causing failures in that project)
    git-svn-id: http://easyb.googlecode.com/svn/trunk@1071 9bebd20b-ac44-0410-9e34-335f4663fabf

--- src/java/org/easyb/BehaviorRunner.java
+++ src/java/org/easyb/BehaviorRunner.java
@@ -75,6 +75,8 @@ public class BehaviorRunner {
 
     executeBehaviors(behaviors);
 
+    ListenerFactory.notifyTestingCompleted();
+
     ResultsAmalgamator r = new ResultsAmalgamator(behaviors);
 
     // this is hideous
--- src/java/org/easyb/listener/ListenerFactory.java
+++ src/java/org/easyb/listener/ListenerFactory.java
@@ -2,6 +2,7 @@ package org.easyb.listener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * listeners that wish to be instanciated when a new broadcast listener is created need to register themselves here.
@@ -9,11 +10,11 @@ import java.util.List;
  * should this be services? like plugins?
  */
 public class ListenerFactory {
-
   private static List<ListenerBuilder> listenerBuilders = new ArrayList<ListenerBuilder>();
+  private static ConcurrentLinkedQueue<ExecutionListener> proxies = new ConcurrentLinkedQueue<ExecutionListener>();
 
   public static void registerBuilder(ListenerBuilder lb) {
-    listenerBuilders.add(lb);
+    listenerBuilders.add(new ListenerBuilderProxy(lb));
   }
 
   public static List<ExecutionListener> getActiveList() {
@@ -24,5 +25,31 @@ public class ListenerFactory {
     }
 
     return listeners;
+  }
+
+  /**
+   * this should be called when all testing is complete to notify any/all execution listeners that all testing
+   * is now complete and we are about to end the session.
+   */
+  public static void notifyTestingCompleted() {
+    for( ExecutionListener el : proxies ) {
+      el.completeTesting();
+    }
+  }
+
+  static class ListenerBuilderProxy implements ListenerBuilder {
+    private ListenerBuilder proxying;
+
+    ListenerBuilderProxy(ListenerBuilder proxying) {
+      this.proxying = proxying;
+    }
+
+    public ExecutionListener get() {
+      ExecutionListener el = proxying.get();
+
+      proxies.add(el); // keep track so when we complete, we can tell them all
+
+      return el;
+    }
   }
 }
