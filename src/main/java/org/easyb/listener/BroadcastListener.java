11d9e4aaab9a6ae459da56fd46c22ca60991101e
John Ferguson Smart <john.smart@wakaleo.com>
2011-05-03 18:16:21 +1000

    Modified the listener architecture so that listeners can be added at any time, by plugins for example. This lets plugins add their own listeners when they are first invoked.

--- src/main/java/org/easyb/listener/BroadcastListener.java
+++ src/main/java/org/easyb/listener/BroadcastListener.java
@@ -5,91 +5,100 @@ import org.easyb.domain.Behavior;
 import org.easyb.result.ReportingTag;
 import org.easyb.result.Result;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class BroadcastListener implements ExecutionListener {
-  private final List<ExecutionListener> listeners;
+    private final List<ExecutionListener> coreListeners;
 
-  private ResultsCollector resultsCollector;
-  private FailureDetector failureDetected;
+    private ResultsCollector resultsCollector;
+    private FailureDetector failureDetected;
 
-  public BroadcastListener() {
-    this.listeners = ListenerFactory.getActiveList();
-    resultsCollector = new ResultsCollector();
-    failureDetected = new FailureDetector();
+    public BroadcastListener() {
+        resultsCollector = new ResultsCollector();
+        failureDetected = new FailureDetector();
 
-    listeners.add(resultsCollector);
-    listeners.add(failureDetected);
-
-    listeners.add(new ConsoleReporterListener());
-  }
-
-  public ResultsCollector getResultsCollector() {
-    return resultsCollector;
-  }
-
-  /**
-   * allows us to get a specific listener from this list
-   *
-   * @param clazz - the class of the listener
-   * @return null if not found or the found listener
-   */
-  public ExecutionListener getTypedListener(Class clazz) {
-    for (ExecutionListener el : listeners) {
-      if (el.getClass() == clazz) {
-        return el;
-      }
+        coreListeners = new ArrayList<ExecutionListener>();
+        coreListeners.add(resultsCollector);
+        coreListeners.add(failureDetected);
+        coreListeners.add(new ConsoleReporterListener());
     }
 
-    return null;
-  }
-
-  public void startBehavior(Behavior behavior) {
-    for (ExecutionListener listener : listeners) {
-      listener.startBehavior(behavior);
+    public List<ExecutionListener> getListeners() {
+        List<ExecutionListener> listeners = new ArrayList<ExecutionListener>();
+        listeners.addAll(coreListeners);
+        listeners.addAll(ListenerFactory.getActiveList());
+        return listeners;
     }
-  }
 
-  public void stopBehavior(BehaviorStep step, Behavior behavior) {
-    for (ExecutionListener listener : listeners) {
-      listener.stopBehavior(step, behavior);
-    }
-  }
 
-  public void tag(ReportingTag tag) {
-    for (ExecutionListener listener : listeners) {
-      listener.tag(tag);
+    public ResultsCollector getResultsCollector() {
+        return resultsCollector;
     }
-  }
 
-  public void startStep(BehaviorStep step) {
-    for (ExecutionListener listener : listeners) {
-      listener.startStep(step);
-    }
-  }
+    /**
+     * allows us to get a specific listener from this list
+     *
+     * @param clazz - the class of the listener
+     * @return null if not found or the found listener
+     */
+    public ExecutionListener getTypedListener(Class clazz) {
+        for (ExecutionListener el : getListeners()) {
+            if (el.getClass() == clazz) {
+                return el;
+            }
+        }
 
-  public void describeStep(String description) {
-    for (ExecutionListener listener : listeners) {
-      listener.describeStep(description);
+        return null;
     }
-  }
 
-  public void completeTesting() {
-    for (ExecutionListener listener : listeners) {
-      listener.completeTesting();
+    public void startBehavior(Behavior behavior) {
+        for (ExecutionListener listener : getListeners()) {
+            listener.startBehavior(behavior);
+        }
     }
-  }
 
-  public void stopStep() {
-    for (ExecutionListener listener : listeners) {
-      listener.stopStep();
+    public void stopBehavior(BehaviorStep step, Behavior behavior) {
+        for (ExecutionListener listener : getListeners()) {
+            listener.stopBehavior(step, behavior);
+        }
     }
-  }
 
-  public void gotResult(Result result) {
-    for (ExecutionListener listener : listeners) {
-      listener.gotResult(result);
+    public void tag(ReportingTag tag) {
+        for (ExecutionListener listener : getListeners()) {
+            listener.tag(tag);
+        }
     }
-  }
+
+    public void startStep(BehaviorStep step) {
+        for (ExecutionListener listener : getListeners()) {
+            listener.startStep(step);
+        }
+    }
+
+    public void describeStep(String description) {
+        for (ExecutionListener listener : getListeners()) {
+            listener.describeStep(description);
+        }
+    }
+
+    public void completeTesting() {
+        for (ExecutionListener listener : getListeners()) {
+            listener.completeTesting();
+        }
+    }
+
+    public void stopStep() {
+        for (ExecutionListener listener : getListeners()) {
+            listener.stopStep();
+        }
+    }
+
+    public void gotResult(Result result) {
+        for (ExecutionListener listener : getListeners()) {
+            listener.gotResult(result);
+        }
+    }
+
 }
